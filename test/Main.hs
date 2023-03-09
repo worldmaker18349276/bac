01a1b986ce8481a25bf388c0c1e35a1ed21ed2f6
worldmaker <worldmaker18349276@gmail.com>
2023-03-09 15:22:23 +0800

    adjust test cone and torus

--- test/Main.hs
+++ test/Main.hs
@@ -5,37 +5,33 @@ import Braider
 
 import Test.HUnit ((@?=), Test (TestList, TestLabel, TestCase), runTestTT, Counts)
 
-cone :: Maybe (Node () ())
-cone = braid $ do
-  y <- knot' []
-  b <- knot' []
-  p <- knot' [y]
-  c <- knot' [y, b]
-  v <- knot' [c, c] // [[0,0], [1,0]] // [[0,1], [1,1]]
-  knot' [p, v] // [[1,0], [1,1]] // [[0,0], [1,0,0]]
-
 test_cone :: Test
 test_cone = TestCase $ do
+  let cone = braid $ do
+        y <- knot' []
+        b <- knot' []
+        p <- knot' [y]
+        c <- knot' [y, b]
+        v <- knot' [c, c] // [[0,0], [1,0]] // [[0,1], [1,1]]
+        knot' [p, v] // [[1,0], [1,1]] // [[0,0], [1,0,0]]
   print cone
   fmap validate cone @?= Just True
 
-torus :: Maybe (Node () ())
-torus = braid $ do
-  t <- knot' []
-  c <- knot' [t, t]
-  c' <- knot' [t, t]
-  p <- knot' [c, c', c, c']
-    // [[0,1], [1,0]]
-    // [[1,1], [2,1]]
-    // [[2,0], [3,1]]
-    // [[3,0], [0,0]]
-  knot' [p]
-    // [[0,0], [0,2]]
-    // [[0,1], [0,3]]
-    -- // [[0,0,0], [0,1,0], [0,2,0], [0,3,0], [0,0,1], [0,1,1], [0,2,1], [0,3,1]]
-
 test_torus :: Test
 test_torus = TestCase $ do
+  let torus = braid $ do
+        t <- knot' []
+        c <- knot' [t, t]
+        c' <- knot' [t, t]
+        p <- knot' [c, c', c, c']
+          // [[0,1], [1,0]]
+          // [[1,1], [2,1]]
+          // [[2,0], [3,1]]
+          // [[3,0], [0,0]]
+        knot' [p]
+          // [[0,0], [0,2]]
+          // [[0,1], [0,3]]
+          -- // [[0,0,0], [0,1,0], [0,2,0], [0,3,0], [0,0,1], [0,1,1], [0,2,1], [0,3,1]]
   print torus
   fmap validate torus @?= Just True
 
