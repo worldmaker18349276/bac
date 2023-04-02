d841ef697ebc7f8228fa8daf867ddb36d414b21a
worldmaker <worldmaker18349276@gmail.com>
2023-04-02 17:52:36 +0800

    add some example for removeMorphism

--- src/BAC/Algorithm.hs
+++ src/BAC/Algorithm.hs
@@ -156,6 +156,42 @@ Examples:
 
 >>> removeMorphism (4, 2) cone
 Nothing
+
+>>> cone' = fromJust $ rewireEdges 0 [((), 1), ((), 4), ((), 3)] cone
+>>> printNode' $ fromJust $ removeMorphism (0, 3) cone'
+- 0->1; 1->2
+  - 0->1
+    &0
+- 0->4; 1->2; 2->6
+  - 0->1
+    *0
+  - 0->2
+
+>>> :{
+printNode' $
+  cone
+  |> removeMorphism (3, 1)
+  |> fromJust
+  |> rewireEdges 3 [((), 4), ((), 2), ((), 3)]
+  |> fromJust
+  |> rewireEdges 0 [((), 1), ((), 3), ((), 4)]
+  |> fromJust
+  |> removeMorphism (3, 4)
+  |> fromJust
+:}
+- 0->1; 1->2
+  - 0->1
+    &0
+- 0->3; 2->2; 3->6
+  - 0->2
+    *0
+  - 0->3
+    &1
+- 0->4; 1->2; 2->6
+  - 0->1
+    *0
+  - 0->2
+    *1
 -}
 removeMorphism ::
   (Symbol, Symbol)   -- ^ The tuple of symbols indicate the morphism to be removed.
