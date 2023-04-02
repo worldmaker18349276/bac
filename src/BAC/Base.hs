0383f3d1a3093720b27797a64352aa2aaf68b8b7
worldmaker <worldmaker18349276@gmail.com>
2023-04-02 17:36:07 +0800

    add an example for rewireEdges

--- src/BAC/Base.hs
+++ src/BAC/Base.hs
@@ -625,18 +625,18 @@ Rewire edges of a given node.
 
 Examples:
 
->>> printNode' $ fromJust $ rewireEdges 0 [((), 1), ((), 2), ((), 3)] cone
+>>> printNode' $ fromJust $ rewireEdges 0 [((), 1), ((), 4), ((), 3)] cone
 - 0->1; 1->2
   - 0->1
     &0
-- 0->2
-  *0
+- 0->4; 1->2; 2->6
+  &1
+  - 0->1
+    *0
+  - 0->2
 - 0->3; 1->4; 2->2; 3->6; 4->4
   - 0->1; 1->2; 2->3
-    &1
-    - 0->1
-      *0
-    - 0->2
+    *1
   - 0->4; 1->2; 2->3
     *1
 
