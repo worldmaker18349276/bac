7a28b3bf2b2dee276dc49dc70d52d0f37ed88809
worldmaker <worldmaker18349276@gmail.com>
2023-04-09 11:45:35 +0800

    simplify Braider, remove DAG

--- src/BAC/Braider.hs
+++ src/BAC/Braider.hs
@@ -1,43 +1,62 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module BAC.Braider (
   BraiderT,
   knot,
+  Path (..),
   (//),
   braidM,
   braid,
 ) where
 
+import Control.Applicative (Alternative (empty))
+import Control.Monad (guard)
+import Control.Monad.Identity (Identity (runIdentity))
+import Control.Monad.State (MonadState (get, put), StateT (runStateT), gets)
+import Control.Monad.Trans.Maybe (MaybeT (runMaybeT))
+import Data.Foldable (find, foldlM)
+import Data.List (nub)
+import Data.List.Extra ((!?))
+import Data.Map.Strict (Map, insert, (!))
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+
 import BAC.Base
 import BAC.Algorithm (expandMergingSymbols)
 import Utils.Utils ((|>), (.>))
-import qualified Utils.DAG as DAG
 
-import Control.Monad (guard)
-import Data.Foldable (foldlM, find)
-import Data.List (nub)
-import Data.Map.Strict ((!))
-import qualified Data.Map.Strict as Map
-import Data.Maybe (listToMaybe, fromMaybe)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Maybe (MaybeT (runMaybeT, MaybeT))
-import Control.Monad.Identity (Identity (runIdentity))
+type DAG p = Map p (Node, [p])
+data BraidState p = BraidState (DAG p) [p]
 
-type BraiderT p m v = DAG.BuilderT Arrow Node p (MaybeT m) v
+newtype BraiderT p m v = BraiderT {runBraiderT :: StateT (BraidState p) (MaybeT m) v}
+  deriving (Functor, Applicative, Alternative, Monad)
 
--- | Make a knot in the Braider.  See 'braid'.
-knot :: (DAG.Pointer p, Monad m) => [p] -> BraiderT p m p
-knot ptrs = do
-  table <- DAG.getTable
+getTable :: Monad m => BraiderT p m (DAG p)
+getTable = BraiderT $ gets (\(BraidState table _) -> table)
+
+insertNode :: (Ord p, Monad m) => Node -> [p] -> BraiderT p m p
+insertNode node children = BraiderT $ do
+  state <- get
+  let BraidState table (p : counter') = state
+  let table' = insert p (node, children) table
+  put $ BraidState table' counter'
+  return p
+
+-- | Make a knot in the Braider.
+knot :: (Enum p, Ord p, Monad m) => [p] -> BraiderT p m p
+knot children = do
+  table <- getTable
 
   let nums =
-        ptrs
+        children
         |> fmap ((table !) .> fst .> symbols .> maximum .> (+ 1))
         |> scanl (+) 1
   let node =
-        ptrs
-        |> fmap ((table !) .> fst)
+        children
+        |> fmap (table !)
+        |> fmap fst
         |> zip nums
         |> fmap (\(num, subnode) -> Arrow {
             dict = subnode |> symbols |> fmap (\a -> (a, num + a)) |> Map.fromList,
@@ -46,27 +65,24 @@ knot ptrs = do
         )
         |> fromEdges
 
-  let children = ptrs |> zip (edges node)
+  insertNode node children
 
-  DAG.node node children
+newtype Path = Path [Int]
 
--- | Merge symbols in the Braider.  See 'braid'.
+-- | Merge symbols in the Braider.
 infixl 2 //
-(//) :: (DAG.Pointer p, Monad m) => BraiderT p m p -> [[Int]] -> BraiderT p m p
-braiding // eqclass = do
+(//) :: (Enum p, Ord p, Monad m) => BraiderT p m p -> [Path] -> BraiderT p m p
+braiding // eqclass_paths = do
   p <- braiding
-  table <- DAG.getTable
+  table <- getTable
   let (node, children) = table ! p
+  let eqclass = eqclass_paths |> fmap (\(Path list) -> list)
 
-  let pathToPointer =
-        foldlM
-        (\p index -> p |> (table !) |> snd |> fmap snd |> drop index |> listToMaybe)
-        p
+  let pathToPointer = foldlM (\p index -> p |> (table !) |> snd |> (!? index)) p
 
-  lift $ MaybeT $ pure $ do
-    ptrs <- eqclass |> traverse pathToPointer
-    guard $ ptrs |> nub |> length |> (<= 1)
-    guard $ ptrs /= [p]
+  ptrs <- eqclass |> traverse (pathToPointer .> maybe empty return)
+  guard $ ptrs |> nub |> length |> (<= 1)
+  guard $ ptrs /= [p]
 
   let pathToArrow = foldl (\arr index -> arr |> extend |> (!! index)) (root node)
 
@@ -82,13 +98,19 @@ braiding // eqclass = do
         let merged_dict = dict arr |> fmap mergeSymbol
         return arr {dict = merged_dict}
   let merged_node = fromEdges merged_edges
-  let merged_children = children |> zip (edges merged_node) |> fmap (fmap snd)
 
-  DAG.node merged_node merged_children
+  insertNode merged_node children
 
--- | Make a BAC by a monad-typed builder.  See 'braid'.
-braidM :: Monad m => (forall p. DAG.Pointer p => BraiderT p m p) -> m (Maybe Node)
-braidM braiding = DAG.buildM braiding |> runMaybeT |> fmap (fmap DAG.value)
+type Pointer = Integer
 
-braid :: (forall p. DAG.Pointer p => BraiderT p Identity p) -> Maybe Node
+-- | Make a BAC by a monad-typed builder.
+braidM :: Monad m => (forall p. (Enum p, Ord p) => BraiderT p m p) -> m (Maybe Node)
+braidM braiding = runMaybeT $ do
+  let init = BraidState mempty [toEnum 0 :: Pointer ..]
+  (p, final) <- runStateT (runBraiderT braiding) init
+  let BraidState table _ = final
+  let (res, _) = table ! p
+  return res
+
+braid :: (forall p. (Enum p, Ord p) => BraiderT p Identity p) -> Maybe Node
 braid braiding = runIdentity (braidM braiding)
