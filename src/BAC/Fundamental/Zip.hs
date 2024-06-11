660f238b3edfb77fa9f260f10e0e102cb34dcdf6
worldmaker <worldmaker18349276@gmail.com>
2024-06-11 23:35:04 +0800

    rewrite Zip

--- src/BAC/Fundamental/Zip.hs
+++ src/BAC/Fundamental/Zip.hs
@@ -9,16 +9,14 @@ module BAC.Fundamental.Zip (
   canonicalizeRootNode,
   canonicalizeArrow,
   canonicalizeNode,
-  -- forwardSymbolTrieUnder,
-  -- backwardSymbolTrieUnder,
   zipSuffixes,
 ) where
 
 import Control.Arrow ((&&&))
 import Control.Monad (guard, mzero)
-import Data.Bifunctor (bimap, first, second)
+import Data.Bifunctor (bimap)
 import Data.List (sort, transpose, nub)
-import Data.List.Extra (allSame, anySame, nubSortOn, nubOn)
+import Data.List.Extra (allSame, anySame, nubSortOn, nubOn, nubSort)
 import Data.Map.Strict ((!))
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust, mapMaybe)
@@ -27,6 +25,7 @@ import Prelude hiding (compare, map)
 import Utils.EqlistSet (canonicalizeGradedEqlistSet)
 import Utils.Utils ((.>), (|>))
 import BAC.Base
+import Data.Foldable.Extra (notNull)
 
 -- $setup
 -- >>> import BAC
@@ -38,20 +37,20 @@ import BAC.Base
 -- | Structural equality, the equality of nodes up to rewiring.
 --   Nodes should have the same symbol list, and equality of child nodes are not checked.
 --   The node with the same structure can be unioned by merging their edges.
-eqStruct :: [BAC] -> Bool
-eqStruct = fmap edgesND .> fmap (fmap dict) .> allSame
+eqStruct :: [BAC e] -> Bool
+eqStruct = fmap edgesND .> fmap (nubSortOn symbol) .> fmap (fmap dict) .> allSame
 
 {- |
 Zip arrows of BACs, which shows the equality of nodes up to rewiring and relabeling.
 Also, the equality of child nodes are not checked.  The correspondence between
 nondecomposable edges of the root nodes should be provided.
 -}
-zipArrows :: [(BAC, [Symbol])] -> Maybe [[Arrow]]
+zipArrows :: Monoid e => [(BAC e, [Symbol])] -> Maybe [[Arrow e]]
 zipArrows nodes_prefix = do
   -- check the given nondecomposable symbols
   guard $
     nodes_prefix
-    |> fmap (edgesND .> fmap symbol .> sort `bimap` sort)
+    |> fmap (edgesND .> fmap symbol .> nubSort `bimap` sort)
     |> all (uncurry (==))
 
   -- ensure that they can be zipped
@@ -93,10 +92,11 @@ Examples:
 >>> fmap elems $ canonicalizeRootNode $ target $ fromJust $ arrow crescent 1
 [[0,1,2,3,4,5,6],[0,1,2,3,6,5,4],[0,3,2,1,4,5,6],[0,3,2,1,6,5,4],[0,4,5,6,1,2,3],[0,6,5,4,1,2,3],[0,4,5,6,3,2,1],[0,6,5,4,3,2,1]]
 -}
-canonicalizeRootNode :: BAC -> [Dict]
+canonicalizeRootNode :: (Monoid e, Ord e) => BAC e -> [Dict]
 canonicalizeRootNode node =
   node
   |> edgesND
+  |> nubSortOn symbol
   |> fmap dict
   |> fmap Map.elems
   |> canonicalizeGradedEqlistSet (arrow node .> fromJust .> target)
@@ -113,10 +113,11 @@ Examples:
 >>> fmap elems $ canonicalizeArrow $ fromJust $ arrow crescent 1
 [[0,1,2,5,3,4,6],[0,3,4,6,1,2,5]]
 -}
-canonicalizeArrow :: Arrow -> [Dict]
+canonicalizeArrow :: Arrow e -> [Dict]
 canonicalizeArrow arr =
   target arr
   |> edgesND
+  |> nubSortOn symbol
   |> fmap dict
   |> fmap Map.elems
   |> canonicalizeGradedEqlistSet (dict arr !)
@@ -132,65 +133,25 @@ Examples:
 >>> fmap elems $ fromJust $ canonicalizeNode crescent 1
 [[0,1,2,5,3,4,6],[0,3,4,6,1,2,5]]
 -}
-canonicalizeNode :: BAC -> Symbol -> Maybe [Dict]
+canonicalizeNode :: Monoid e => BAC e -> Symbol -> Maybe [Dict]
 canonicalizeNode node tgt = do
   guard $ tgt /= base
   tgt_arr <- arrow node tgt
   let keys =
         tgt
         |> suffixND node
+        |> nubSortOn symbol2
         |> fmap (snd .> dict .> fmap (: []))
         |> foldl (Map.unionWith (++)) Map.empty
   return $
     target tgt_arr
     |> edgesND
+    |> nubSortOn symbol
     |> fmap dict
     |> fmap Map.elems
     |> canonicalizeGradedEqlistSet (keys !)
     |> fmap (concat .> nub .> (base :))
     |> fmap ((`zip` [base..]) .> Map.fromList)
-
-{- |
-Collect all maximum chains to a node into a trie.
-It is a tree such that its paths correspond to maximum chains, which is represented as a
-sequence of pairs of symbols, and each pair of symbols indicates a nondecomposable
-morphism.  Just like BAC, the nodes of this trie is implicitly shared.
-
-Examples:
-
->>> putStr $ showTree $ fromJust $ forwardSymbolTrieUnder 6 cone
-{(0,3):{(3,1):{(4,2):{}},(3,4):{(4,2):{}}}}
--}
-forwardSymbolTrieUnder :: Symbol -> BAC -> Maybe (Tree (Symbol, Symbol))
-forwardSymbolTrieUnder sym = fromReachable res0 . foldUnder sym \curr results ->
-  edges (target curr) `zip` results
-  |> mapMaybe (second (fromReachable res0) .> sequence)
-  |> fmap (first symbol)
-  |> filter (fst .> nondecomposable (target curr))
-  |> fmap (first (symbol curr,))
-  |> Map.fromList
-  |> Tree
-  where
-  res0 = Tree Map.empty
-
-{- |
-Collect all maximum chains to a node into a reversed trie.
-It is a tree such that its paths correspond to the reverse of maximum chains, which is
-represented as a sequence of pairs of symbols, and each pair of symbols indicates a
-nondecomposable morphism.  Just like BAC, the nodes of this trie is implicitly shared.
-
-Examples:
-
->>> putStr $ showTree $ fromJust $ backwardSymbolTrieUnder 6 cone
-{(4,2):{(3,1):{(0,3):{}},(3,4):{(0,3):{}}}}
--}
-backwardSymbolTrieUnder :: Symbol -> BAC -> Maybe (Tree (Symbol, Symbol))
-backwardSymbolTrieUnder sym = cofoldUnder sym \_curr results ->
-  results
-  |> filter (fst .> \(arr1, arr2) -> nondecomposable (target arr1) (symbol arr2))
-  |> fmap (first symbol2)
-  |> Map.fromList
-  |> Tree
 
 {- |
 Zip all suffixes of given nodes.
@@ -208,50 +169,66 @@ Examples:
 []
 -}
 zipSuffixes ::
-  [(Symbol, [(Symbol, Symbol)])]
-  -- ^ The symbols to zip and the keys to classify nondecomposable incoming edges.
-  -> BAC
-  -> Maybe [(Arrow, [Arrow])]
-zipSuffixes [] _ = Just []
-zipSuffixes tgts_suffix node = do
+  Monoid e
+  => BAC e
+  -> [(Symbol, [Symbol])]
+  -- ^ The group of list of nondecomposable incoming edges to zip.
+  -> Maybe [(Symbol, [Symbol])]
+zipSuffixes node suffixes = do
+  -- the i-th arrow to merge for each group should have the same target
+  guard $ suffixes |> all (snd .> notNull)
+  guard $ suffixes |> fmap (snd .> length) |> allSame
+  tgt_arrs <- suffixes
+    |> fmap sequence
+    |> traverse (traverse (arrow2 node .> fmap (uncurry join)))
   guard $
-    tgts_suffix
-    |> fmap (suffixND node .> fmap symbol2 `bimap` sort)
+    tgt_arrs
+    |> transpose
+    |> all (fmap symbol .> allSame)
+  guard $
+    tgt_arrs
+    |> head
+    |> fmap symbol
+    |> anySame
+    |> not
+
+  -- evrey nondecomposable incoming edges should be zipped exactly once
+  guard $ head tgt_arrs
+    |> fmap (symbol .> suffixND node .> fmap symbol2 .> nubSort)
+    |> zip (suffixes |> fmap sequence |> transpose |> fmap sort)
     |> all (uncurry (==))
-  guard $ tgts_suffix |> fmap (snd .> fmap fst) |> allSame
 
-  let zipped_nd_suffix =
-        tgts_suffix
-        |> fmap snd
-        |> transpose
-        |> fmap ((head .> fst) &&& fmap snd)
-
-  let tgts = tgts_suffix |> fmap fst
+  let tgts = head tgt_arrs |> fmap symbol
 
   fromJust $ fromReachable (Just []) $
     node |> foldUnder (head tgts) \curr results -> do
       results' <- results |> traverse sequence
 
-      let collapse = nubSortOn (fmap symbol) do
+      -- collapse is a set of merge lists of symbols
+      let collapse = nubSort do
             (lres, edge) <- results' `zip` edges (target curr)
             let sym0 = symbol curr
             let sym = symbol (curr `join` edge)
             case lres of
+              -- map merge lists of children into this node
               AtInner res ->
                 res
-                |> filter (fst .> symbol .> (== sym))
-                |> fmap (snd .> fmap (join edge))
+                |> filter (fst .> (== sym))
+                |> fmap (snd .> fmap (dict edge !))
+              -- user specified merge lists
               _ | sym `elem` tgts ->
-                zipped_nd_suffix
+                suffixes
                 |> filter (fst .> (== sym0))
-                |> fmap (snd .> fmap (arrow (target curr) .> fromJust))
+                |> fmap snd
               _otherwise -> mzero
 
-      guard $ collapse |> transpose |> all (anySame .> not)
+      -- merge lists should have distinct symbols
+      guard $ collapse |> concat |> anySame |> not
 
+      -- append to result
       return $
         results'
         |> mapMaybe fromInner
         |> concat
-        |> nubOn (symbol `bimap` fmap symbol)
-        |> (++ fmap (curr,) collapse)
+        |> nub
+        |> (++ fmap (symbol curr,) collapse)
